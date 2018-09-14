8f72b2d9d9c5dede41f97b115d213ce00a56bdeb
Anuradha Kumari <anuradha.kumari@globant.com>
2018-09-14 16:59:06 +0530

    build a bot - ongoing

---
+++ src/shared/abc.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="header">
+            <span v-if="open" @click="open=!open">Collapse</span>
+            <span v-else="!open" @click="open=!open">Expand</span>
+
+        </div>
+
+        <slot v-if="open">
+            <div>Default</div>
+        </slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CollapsibleSection',
+        data() {
+            return {
+                open:true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.header {
+    background-color: gray;
+    padding: 3px;
+    cursor: pointer;
+}
+</style>
